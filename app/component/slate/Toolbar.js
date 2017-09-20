f16070a3c7c85625ed2a8dca861ab413d4066035
bryan <bryan@localhost.localdomain>
2017-09-20 14:39:17 -0500

    styling for toolbar

--- app/component/slate/Toolbar.js
+++ app/component/slate/Toolbar.js
@@ -8,7 +8,24 @@ export default (props) => {
       <span className="btn btn-default fa fa-italic"></span>
       <span className="btn btn-default fa fa-underline"></span>
       <span className="btn btn-default fa fa-code"></span>
-      <span className="btn btn-default fa fa-header"></span>
+      <div className="dropdown c-inline">
+        <button
+          className="btn btn-default dropdown-toggle"
+          type="button"
+          id="dropdownMenuButton"
+          data-toggle="dropdown"
+        >
+        <span className="fa fa-header"></span>
+        &nbsp;
+        <span className="fa fa-caret-down"></span>
+        </button>
+        <div className="dropdown-menu">
+          <span className="dropdown-item" href="#"><h1>AaBbCc</h1>Heading 1</span>
+          <span className="dropdown-item" href="#"><h2>AaBbCc</h2>Heading 2</span>
+          <span className="dropdown-item" href="#"><h3>AaBbCc</h3>Heading 3</span>
+          <span className="dropdown-item" href="#"><h4>AaBbCc</h4>Heading 4</span>
+        </div>
+      </div>
       <span className="btn btn-default fa fa-quote-right"></span>
       <span className="btn btn-default fa fa-list-ol"></span>
       <span className="btn btn-default fa fa-list-ul"></span>
